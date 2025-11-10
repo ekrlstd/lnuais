3dfa8cc2fcab2b863495b7acda7270df1239086d
ekrlstd <ebbekarlstad@proton.me>
2025-11-10 03:36:29 +0100

    background changes, added a navbar with blur effects

---
+++ scripts/index.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  // Default to index.html for root path
+  let filePath = req.url === "/" ? "/index.html" : req.url;
+
+  // Build full path from project root
+  filePath = path.join(__dirname, "..", filePath);
+
+  // Get file extension
+  const extname = path.extname(filePath);
+
+  // Set content type based on file extension
+  const contentTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".svg": "image/svg+xml",
+  };
+
+  const contentType = contentTypes[extname] || "text/plain";
+
+  // Read and serve the file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 - File Not Found</h1>", "utf-8");
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`, "utf-8");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});

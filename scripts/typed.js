ff9c6e75779353dc96c95d85107b7ec851ec0d17
ekrlstd <ebbekarlstad@proton.me>
2025-11-10 16:02:31 +0100

    add hero content, typing animations, and general styling and responsiveness

---
+++ scripts/typed.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typing = new Typed(".text", {
+    strings: [
+      // here we can add more strings if we want!
+      "advancing AI knowledge.",
+      "fostering innovation.",
+      "connecting enthusiasts.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 40,
+    loop: true,
+    showCursor: false,
+  });
+});
